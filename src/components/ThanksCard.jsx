d24a177646fd94fa8680a17d04ce772135f75697
gabenerys <gabe.nery@live.com>
2022-04-16 11:54:06 -0300

    feat: finalizing thank you screen

--- src/components/ThanksCard.jsx
+++ src/components/ThanksCard.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import thanks from '../images/thanks.svg';
 import Footer from './Footer';
+import myContext from '../Context/myContext';
 import './ThanksCard.css';
 
 const ThanksCard = () => {
+  const { value } = useContext(myContext);
   return (
     <>
       <main>
         <section className="ThanksCard">
         <img src={ thanks } alt="" />
-        <p className='ratingNote'>You selected out of 5</p>
+        <p className='ratingNote'>You selected { value } out of 5</p>
         <div className="paragraph">
           <h1>Thank you!</h1>
           <p> 
